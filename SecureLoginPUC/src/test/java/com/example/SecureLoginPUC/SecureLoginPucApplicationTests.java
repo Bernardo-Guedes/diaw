a82a3e2d69a683e0a6816afcde31bea54b81c692
Bernardo Guedes <bguedessilveira@gmail.com>
2026-09-12 12:02:41 -0300

    feat: add initial HTML prototypes

---
+++ SecureLoginPUC/src/test/java/com/example/SecureLoginPUC/SecureLoginPucApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SecureLoginPUC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes={com.example.SecureLoginPUC.application.SecureLoginPucApplication.class})
+class SecureLoginPucApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
